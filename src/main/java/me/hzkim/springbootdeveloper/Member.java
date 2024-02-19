7a6d42e8deec82862aac912972f131e73c9c4342
kros235 <kros235@naver.com>
2024-02-19 14:05:10 +0900

    2024-02-19
    ~ Chapter #05

--- src/main/java/me/hzkim/springbootdeveloper/Member.java
+++ src/main/java/me/hzkim/springbootdeveloper/Member.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity                                                 //  엔티티로 지정
+// @Entity(name = "member_list")                        //  member_list라는 이름을 가진 테이블과 매핑
 @NoArgsConstructor(access = AccessLevel.PROTECTED)      //  기본 생성자
 @AllArgsConstructor
 @Getter
--- src/main/java/me/hzkim/springbootdeveloper/MemberRepository.java
+++ src/main/java/me/hzkim/springbootdeveloper/MemberRepository.java
@@ -1,5 +1,6 @@
 package me.hzkim.springbootdeveloper;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
